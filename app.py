ee548d91625365d126f62770845f6cdf451e808b
Carmo <87614629+joaoCarmo10@users.noreply.github.com>
2023-04-25 13:54:07 -0300

    coomit

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import dash_bootstrap_components as dbc
 from dash import Dash, html, dcc
+#import sass
 
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
+app = Dash(__name__, external_stylesheets=[dbc.themes.QUARTZ])
 server = app.server
 
-# Menu lateral
-
-#inserindo a navbar
 navbar = dbc.NavbarSimple(
     children=[
         dbc.NavItem(dbc.NavLink("Home")),
@@ -16,45 +14,15 @@ navbar = dbc.NavbarSimple(
         dbc.NavItem(dbc.NavLink("Contato")) 
     ],
     brand="João Pedro",
-    brand_href="#",
+    #brand_href="#",
+    className="navbar-transparent",
     color="#000000",
     #color='white',
     dark=True,
     sticky='top',
-    style={'borderBottom': 'none', 'height': '60px'
+    #style={'borderBottom': 'none', 'height': '60px', 'transparent':'true'
            #'backgroundColor': 'black', 'opacity':'0.5'
-    }, 
-)
-
-card = dbc.Card(
-    dbc.CardBody(
-        [
-            html.H4("JOÃO PEDRO DO CARMO", className="card-title")
-            ,html.P("ENGENHEIRO DE PRODUÇÃO, ANALISTA DE CUSTOS, APAIXONADO POR DADOS E TRIATLETA.")        
-        ]
-    )
-)
-
-# Conteúdo principal
-content = dbc.Container(
-    dbc.Row(
-        dbc.Col(
-            card,
-            width={"size": 10, "offset": 1},
-            className="mt-5",
-            style={
-                "height": "65vh",
-                "display": "flex",
-                "align-items": "center",
-                "justify-content": "center",
-            }
-        )
-    ),
-    fluid=True,
-    style={
-        "height": "92vh",
-        #"background-image": "linear-gradient(to bottom right, #007bff, #000000)",
-    }
+    #}, 
 )
 
 footer = dbc.Container(    
@@ -82,33 +50,66 @@ footer = dbc.Container(
 )
 
 
-grid = html.H4( "Bem-vindo(a)",style={"text-align": "center", "margin-top":"50px"})
 grid1 = html.Div(
     dbc.Row([
-        dbc.Col(
-            html.P("Meu nome é João Pedro do Carmo Costa Santos, sou Engenheiro de Produção, atualmente trabalhando como Desenvolvedor BI e como Consultor Autônomo de Business Intelligence.")
+        dbc.Col([
+            html.A(html.Img(src = "https://media.licdn.com/dms/image/D4D03AQFP4olXaQdzGw/profile-displayphoto-shrink_800_800/0/1680175468993?e=1687996800&v=beta&t=xDFEeXdRTFn5plY_xP9ZTv-Xs703V9CBfAHsJSOFjlo", height="100px",style={"margin-left": "10px", "border-radius": "50%", 'align':'center'})),
+            html.P(html.H5( "Bem-vindo(a), ",style={"text-align": "center", "margin-top":"50px"})),
+            html.P("Meu nome é João Pedro do Carmo Costa Santos, sou Engenheiro de Produção, atualmente trabalhando como Analista de Custos e como Consultor Autônomo de Business Intelligence.")]
         ,width={
-                'size': 10, 
+                'size': 12, 
                 'order': 1, 
-                'offset': 1,
-                }, md=4),
-        dbc.Col(
-            html.H1("Foto Profissional")
-            ,width={
-                'size':10,
-                'order':2,
-                'offset':1
-            }, md=4
-        )
+                #'offset': 2,
+                }, md=12
+            ),
+        #dbc.Col(
+           # html.A(html.Img(src = "https://media.licdn.com/dms/image/D4D03AQFP4olXaQdzGw/profile-displayphoto-shrink_800_800/0/1680175468993?e=1687996800&v=beta&t=xDFEeXdRTFn5plY_xP9ZTv-Xs703V9CBfAHsJSOFjlo", height="200px",style={"margin-left": "10px", "border-radius": "50%"}))
+            #,width={
+             #   'size':12,
+              #  'order':2,
+               # 'offset':1
+            #}, md=4
+        #)
     ]), style={'display': 'flex', 'align-items': 'center', 'justify-content': 'center', 'height': '50vh', 'text-align':'justify'}
     
 )
 
+card = dbc.Card(
+    dbc.CardBody(
+        grid1
+    ),style={"background-image": "linear-gradient(to bottom right, #007bff, #000000)"}
+    
+)
+
+
+content = dbc.Container(
+    dbc.Row(
+        dbc.Col(
+            card,
+            width={"size": 6, 
+                   "offset": 3,
+                   "align":"center"
+                   },
+            className="mt-5",
+            style={
+                "height": "65vh",
+                "display": "flex",
+                "align-items": "center",
+                "justify-content": "center",
+            }
+        )
+    ),
+    fluid=True,
+    style={
+        "height": "92vh",
+        "background-image": "linear-gradient(to bottom right, #007bff, #000000)",
+    }
+)
+
 app.layout = html.Div([navbar, 
-                       grid,
-                       grid1,
+                       content,
                        footer
-                       ])
+                       ], style={"background-image": "linear-gradient(to bottom right, #007bff, #000000)"})
 
 if __name__ == "__main__":
     app.run_server(port=8000, debug=True)
