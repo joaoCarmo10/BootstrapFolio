14bf47723d7bd0c9c799b60b10aa4e9d9b7e1a14
Carmo <87614629+joaoCarmo10@users.noreply.github.com>
2023-04-27 15:28:39 -0300

    changes

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import dash_bootstrap_components as dbc
 from dash import Dash, html, dcc
-#import sass
 
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.QUARTZ])
+app = Dash(__name__, external_stylesheets=[dbc.themes.QUARTZ, "/Assets/style.css"])
 server = app.server
 
 navbar = dbc.NavbarSimple(
@@ -14,15 +13,13 @@ navbar = dbc.NavbarSimple(
         dbc.NavItem(dbc.NavLink("Contato")) 
     ],
     brand="João Pedro",
-    #brand_href="#",
     className="navbar-transparent",
-    color="#000000",
-    #color='white',
+    #color='#000000',
     dark=True,
     sticky='top',
-    #style={'borderBottom': 'none', 'height': '60px', 'transparent':'true'
-           #'backgroundColor': 'black', 'opacity':'0.5'
-    #}, 
+    style={'borderBottom': 'none', 'height': '50px', 
+           #'backgroundColor': '#000000'
+           }, 
 )
 
 footer = dbc.Container(    
@@ -32,84 +29,64 @@ footer = dbc.Container(
                 html.P("Desenvolvido por João Pedro do Carmo, 2023 ", className="text-muted", style={"text-align": "center", "margin-top":"15px"}),
                 html.A(html.Img(src=" https://cdn-icons-png.flaticon.com/512/3955/3955024.png ", height="30px", style={"margin-left": "10px"}), href="https://www.instagram.com/joaocarmo.co/", target="_blank"),
                 html.A(html.Img(src="https://cdn-icons-png.flaticon.com/512/3955/3955051.png", height="30px",style={"margin-left": "10px"}), href="https://www.linkedin.com/in/joaodocarmo95/", target="_blank", className="ml-auto")
-            ], 
-                         
-            className="d-flex align-items-center justify-content-center"),
+            ], className="d-flex align-items-center justify-content-center"),
             className="mt-2",
             style={"padding": "5px"}
         ),
     ),
     fluid=True,
     style={
-         #"background-color": "#f8f9fa",
-        #"border-top": "1px solid #000000",
         "position": "fixed",
         "bottom": "0",
         "width": "100%",            
     }
 )
 
-
-grid1 = html.Div(
+grid1 = dbc.Container(
     dbc.Row([
         dbc.Col([
-            html.A(html.Img(src = "https://media.licdn.com/dms/image/D4D03AQFP4olXaQdzGw/profile-displayphoto-shrink_800_800/0/1680175468993?e=1687996800&v=beta&t=xDFEeXdRTFn5plY_xP9ZTv-Xs703V9CBfAHsJSOFjlo", height="100px",style={"margin-left": "10px", "border-radius": "50%", 'align':'center'})),
-            html.P(html.H5( "Bem-vindo(a), ",style={"text-align": "center", "margin-top":"50px"})),
-            html.P("Meu nome é João Pedro do Carmo Costa Santos, sou Engenheiro de Produção, atualmente trabalhando como Analista de Custos e como Consultor Autônomo de Business Intelligence.")]
-        ,width={
-                'size': 12, 
-                'order': 1, 
-                #'offset': 2,
-                }, md=12
-            ),
-        #dbc.Col(
-           # html.A(html.Img(src = "https://media.licdn.com/dms/image/D4D03AQFP4olXaQdzGw/profile-displayphoto-shrink_800_800/0/1680175468993?e=1687996800&v=beta&t=xDFEeXdRTFn5plY_xP9ZTv-Xs703V9CBfAHsJSOFjlo", height="200px",style={"margin-left": "10px", "border-radius": "50%"}))
-            #,width={
-             #   'size':12,
-              #  'order':2,
-               # 'offset':1
-            #}, md=4
-        #)
-    ]), style={'display': 'flex', 'align-items': 'center', 'justify-content': 'center', 'height': '50vh', 'text-align':'justify'}
-    
-)
+            html.P(html.H5("Bem-vindo(a), ", style={"text-align": "center", "margin-top": "50px"})),
+            html.P("Meu nome é João Pedro do Carmo Costa Santos, sou Engenheiro de Produção, atualmente trabalhando como Analista de Custos e como Consultor Autônomo de Business Intelligence.")
+        ]),
+        dbc.Col([
+            html.A(html.Img(src="https://media.licdn.com/dms/image/D4D03AQFP4olXaQdzGw/profile-displayphoto-shrink_800_800/0/1680175468993?e=1687996800&v=beta&t=xDFEeXdRTFn5plY_xP9ZTv-Xs703V9CBfAHsJSOFjlo", height="100px", style={"margin-left": "10px", "border-radius": "50%", "text-align": "center", "margin-top": "50px"}))
+        ], style={'display': 'flex', 'align-items': 'center', 'justify-content': 'center'})
+    ], style={'display': 'flex', 'align-items': 'center', 'justify-content': 'center', 'height': '50vh', 'text-align': 'justify'}
+))
+
 
 card = dbc.Card(
-    dbc.CardBody(
-        grid1
-    ),style={"background-image": "linear-gradient(to bottom right, #007bff, #000000)"}
-    
+    dbc.CardBody(grid1),
+    className='card-container',
+    #style={#"background-image": "linear-gradient(to bottom right, #007bff, #000000)", 
+     #     'height':'80%', 'margin-left':'30px'}
 )
+
 
 
 content = dbc.Container(
     dbc.Row(
         dbc.Col(
             card,
-            width={"size": 6, 
-                   "offset": 3,
-                   "align":"center"
-                   },
-            className="mt-5",
-            style={
-                "height": "65vh",
-                "display": "flex",
-                "align-items": "center",
-                "justify-content": "center",
-            }
+            width={"size": 12, "md": 6},
+            className="mt-5 d-flex align-items-center justify-content-center",
+            style={"height": "65vh"}
         )
     ),
     fluid=True,
     style={
-        "height": "92vh",
-        "background-image": "linear-gradient(to bottom right, #007bff, #000000)",
+        "height": "93.5vh",
+        #"background-image": "linear-gradient(to bottom right, #007bff, #000000)",
+        "display": "flex"
     }
 )
 
-app.layout = html.Div([navbar, 
-                       content,
-                       footer
-                       ], style={"background-image": "linear-gradient(to bottom right, #007bff, #000000)"})
+app.layout = html.Div([navbar,                        
+                       content,                       
+                       footer                       ], 
+                       style={#"background-image": "linear-gradient(to bottom right, #007bff, #000000)",
+                              "min-height": "100vh",
+                              "background-size":"cover"})
 
 if __name__ == "__main__":
     app.run_server(port=8000, debug=True)
